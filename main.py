39559293bed1e00086f7611df02ef040d48e035e
mahdiyekordzaboli-ux <mahdiye.kord.zaboli@gmail.com>
2026-08-28 07:49:02 +0330

    Create main.py

---
+++ main.py
@@ -0,0 +1,1 @@
+from fastapi import FastAPI, Query، from fastapi.staticfiles import StaticFiles، from fastapi.templating import Jinja2Templates، از pathlib, از fastapi import FastAPI, HTTPException, Query
